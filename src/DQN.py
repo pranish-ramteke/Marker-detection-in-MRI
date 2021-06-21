5bcfe14bb4485240a277dd7b209c491135218b85
Pranish Ramteke <50505217+pranish-ramteke@users.noreply.github.com>
2021-06-22 05:01:19 +0530

    Add files via upload

---
+++ src/DQN.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: DQN.py
+# Author: Amir Alansary <amiralansary@gmail.com>
+
+import warnings
+from evaluator import Evaluator
+from logger import Logger
+from trainer import Trainer
+from DQNModel import DQN
+from medical import MedicalPlayer, FrameStack
+import argparse
+import os
+import torch
+import numpy as np
+
+
+def warn(*args, **kwargs):
+    pass
+
+
+warnings.warn = warn
+warnings.simplefilter("ignore", category=PendingDeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+###############################################################################
+# BREAKOUT (84,84) - MEDICAL 2D (60,60) - MEDICAL 3D (26,26,26)
+IMAGE_SIZE = (45, 45, 45)
+# how many frames to keep
+# in other words, how many observations the network can see
+FRAME_HISTORY = 4
+###############################################################################
+
+
+def get_player(directory=None, files_list=None, landmark_ids=None, viz=False,
+               task="play", file_type="brain", saveGif=False, saveVideo=False,
+               multiscale=True, history_length=20, agents=1, logger=None):
+    env = MedicalPlayer(
+        directory=directory,
+        screen_dims=IMAGE_SIZE,
+        viz=viz,
+        saveGif=saveGif,
+        saveVideo=saveVideo,
+        task=task,
+        files_list=files_list,
+        file_type=file_type,
+        landmark_ids=landmark_ids,
+        history_length=history_length,
+        multiscale=multiscale,
+        agents=agents,
+        logger=logger)
+    if task != "train":
+        # in training, env will be decorated by ExpReplay, and history
+        # is taken care of in expreplay buffer
+        # otherwise, FrameStack modifies self.step to save observations into a
+        # queue
+        env = FrameStack(env, FRAME_HISTORY, agents)
+    return env
+
+###############################################################################
+###############################################################################
+
+def set_reproducible(seed):
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--load', help='Path to the model to load')
+    parser.add_argument(
+        '--task',
+        help='''task to perform,
+                must load a pretrained model if task is "play" or "eval"''',
+        choices=['play', 'eval', 'train'], default='train')
+    parser.add_argument(
+        '--file_type', help='Type of the training and validation files',
+        choices=['brain', 'cardiac', 'fetal'], default='train')
+    parser.add_argument(
+        '--files', type=argparse.FileType('r'), nargs='+',
+        help="""Filepath to the text file that contains list of images.
+                Each line of this file is a full path to an image scan.
+                For (task == train or eval) there should be two input files
+                ['images', 'landmarks']""")
+    parser.add_argument(
+        '--val_files', type=argparse.FileType('r'), nargs='+',
+        help="""Filepath to the text file that contains list of validation
+                images. Each line of this file is a full path to an image scan.
+                For (task == train or eval) there should be two input files
+                ['images', 'landmarks']""")
+    parser.add_argument('--saveGif', help='Save gif image of the game',
+                        action='store_true', default=False)
+    parser.add_argument('--saveVideo', help='Save video of the game',
+                        action='store_true', default=False)
+    parser.add_argument(
+        '--log_dir', help='Store logs in this directory during training.',
+        default='runs', type=str)
+    parser.add_argument(
+        '--log_comment', help='Suffix appended to the name of the log folder name, which is the current time.',
+        default='', type=str)
+    parser.add_argument(
+        '--landmarks', nargs='*', help='Landmarks to use in the images',
+        type=int, default=[1])
+    parser.add_argument(
+        '--model_name', help='Models implemented are: Network3d, CommNet',
+        default="CommNet", choices=['CommNet', 'Network3d'], type=str)
+    parser.add_argument(
+        '--batch_size', help='Size of each batch', default=64, type=int)
+    parser.add_argument(
+        '--memory_size',
+        help="""Number of transitions stored in exp replay buffer.
+                If too much is allocated training may abruptly stop.""",
+        default=1e5, type=int)
+    parser.add_argument(
+        '--init_memory_size',
+        help='Number of transitions stored in exp replay before training',
+        default=3e4, type=int)
+    parser.add_argument(
+        '--discount',
+        help='Discount factor used in the Bellman equation',
+        default=0.9, type=float)
+    parser.add_argument(
+        '--lr',
+        help='Starting learning rate',
+        default=1e-3, type=float)
+    parser.add_argument(
+        '--scheduler_gamma',
+        help='Multiply the learning rate by this value every scheduler_step_size epochs',
+        default=0.5, type=float)
+    parser.add_argument(
+        '--scheduler_step_size',
+        help='Every scheduler_step_size epochs, the learning rate is multiplied by scheduler_gamma',
+        default=100, type=int)
+    parser.add_argument(
+        '--max_episodes', help='"Number of episodes to train for"',
+        default=1e5, type=int)
+    parser.add_argument(
+        '--steps_per_episode', help='Maximum steps per episode',
+        default=200, type=int)
+    parser.add_argument(
+        '--target_update_freq',
+        help='Number of epochs between each target network update',
+        default=10, type=int)
+    parser.add_argument(
+        '--save_freq', help='Saves network every save_freq steps',
+        default=1000, type=int)
+    parser.add_argument(
+        '--delta',
+        help="""Amount to decreases epsilon each episode,
+                for the epsilon-greedy policy""",
+        default=1e-4, type=float)
+    parser.add_argument(
+        '--viz', help='Size of the window, None for no visualisation',
+        default=0.01, type=float)
+    parser.add_argument(
+        '--multiscale',
+        help='Reduces size of voxel around the agent when it oscillates',
+        dest='multiscale', action='store_true')
+    parser.set_defaults(multiscale=False)
+    parser.add_argument(
+        '--write', help='Saves the training logs', dest='write',
+        action='store_true')
+    parser.set_defaults(write=False)
+    parser.add_argument(
+        '--team_reward', help='Refers to adding the (potentially weighted) average reward of all agents to their individiual rewards', 
+        choices=[None, 'mean', 'attention'], default=None)
+    parser.add_argument(
+        '--attention', help='Use attention for communication channel in C-MARL/CommNet', dest='attention',
+        action='store_true')
+    parser.set_defaults(attention=False)
+    parser.add_argument(
+        '--train_freq',
+        help="""Number of agent steps between each training step on one
+                mini-batch""",
+        default=1, type=int)
+    parser.add_argument(
+        '--seed',
+        help="Random seed for both training and evaluating. If none is provided, no seed will be set", type=int)
+    parser.add_argument(
+        '--fixed_spawn', nargs='*',  type=float,
+        help='Starting position of the agents during rollout. Randomised if not specified.',)
+    args = parser.parse_args()
+
+    agents = len(args.landmarks)
+
+    # check valid number of agents:
+    assert agents > 0
+
+    # initial memory size must be less or equal than memory size
+    init_memory_size = min(args.init_memory_size, args.memory_size)
+
+    # check input files
+    if args.task == 'play':
+        error_message = f"""Wrong input files {len(args.files)} for {args.task}
+                            task - should be 1 \'images.txt\' """
+        assert len(args.files) == 1, (error_message)
+    else:
+        error_message = f"""Wrong input files {len(args.files)} for
+                            {args.task} task - should be 2 [\'images.txt\',
+                            \'landmarks.txt\'] """
+        assert len(args.files) == 2, (error_message)
+
+    if args.seed is not None:
+        set_reproducible(args.seed)
+
+    logger = Logger(args.log_dir, args.write, args.save_freq, comment=args.log_comment)
+
+    if args.task != 'train':
+        dqn = DQN(agents, frame_history=FRAME_HISTORY, logger=logger,
+                  type=args.model_name, collective_rewards=args.team_reward, attention=args.attention)
+        model = dqn.q_network
+        model.load_state_dict(torch.load(args.load, map_location=model.device))
+        environment = get_player(files_list=args.files,
+                                 file_type=args.file_type,
+                                 landmark_ids=args.landmarks,
+                                 saveGif=args.saveGif,
+                                 saveVideo=args.saveVideo,
+                                 task=args.task,
+                                 agents=agents,
+                                 viz=args.viz,
+                                 logger=logger)
+        evaluator = Evaluator(environment, model, logger, agents,
+                              args.steps_per_episode)
+        evaluator.play_n_episodes(fixed_spawn=args.fixed_spawn)
+    else:  # train model
+        environment = get_player(task='train',
+                                 files_list=args.files,
+                                 file_type=args.file_type,
+                                 landmark_ids=args.landmarks,
+                                 agents=agents,
+                                 viz=args.viz,
+                                 multiscale=args.multiscale,
+                                 logger=logger)
+        eval_env = None
+        if args.val_files is not None:
+            eval_env = get_player(task='eval',
+                                  files_list=args.val_files,
+                                  file_type=args.file_type,
+                                  landmark_ids=args.landmarks,
+                                  agents=agents,
+                                  logger=logger)
+        trainer = Trainer(environment,
+                          eval_env=eval_env,
+                          batch_size=args.batch_size,
+                          image_size=IMAGE_SIZE,
+                          frame_history=FRAME_HISTORY,
+                          update_frequency=args.target_update_freq,
+                          replay_buffer_size=args.memory_size,
+                          init_memory_size=init_memory_size,
+                          gamma=args.discount,
+                          steps_per_episode=args.steps_per_episode,
+                          max_episodes=args.max_episodes,
+                          delta=args.delta,
+                          logger=logger,
+                          model_name=args.model_name,
+                          train_freq=args.train_freq,
+                          team_reward=args.team_reward,
+                          attention=args.attention,
+                          lr=args.lr,
+                          scheduler_gamma=args.scheduler_gamma,
+                          scheduler_step_size=args.scheduler_step_size
+                         ).train()
---
+++ src/DQNModel.py
@@ -0,0 +1,365 @@
+import torch
+import torch.nn as nn
+
+class Network3D(nn.Module):
+
+    def __init__(self, agents, frame_history, number_actions, xavier=True):
+        super(Network3D, self).__init__()
+
+        self.agents = agents
+        self.frame_history = frame_history
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
+
+        self.conv0 = nn.Conv3d(
+            in_channels=frame_history,
+            out_channels=32,
+            kernel_size=(5, 5, 5),
+            padding=1).to(
+            self.device)
+        self.maxpool0 = nn.MaxPool3d(kernel_size=(2, 2, 2)).to(self.device)
+        self.prelu0 = nn.PReLU().to(self.device)
+        self.conv1 = nn.Conv3d(
+            in_channels=32,
+            out_channels=32,
+            kernel_size=(5, 5, 5),
+            padding=1).to(
+            self.device)
+        self.maxpool1 = nn.MaxPool3d(kernel_size=(2, 2, 2)).to(self.device)
+        self.prelu1 = nn.PReLU().to(self.device)
+        self.conv2 = nn.Conv3d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(4, 4, 4),
+            padding=1).to(
+            self.device)
+        self.maxpool2 = nn.MaxPool3d(kernel_size=(2, 2, 2)).to(self.device)
+        self.prelu2 = nn.PReLU().to(self.device)
+        self.conv3 = nn.Conv3d(
+            in_channels=64,
+            out_channels=64,
+            kernel_size=(3, 3, 3),
+            padding=0).to(
+            self.device)
+        self.prelu3 = nn.PReLU().to(self.device)
+
+        self.fc1 = nn.ModuleList(
+            [nn.Linear(in_features=512, out_features=256).to(
+                self.device) for _ in range(self.agents)])
+        self.prelu4 = nn.ModuleList(
+            [nn.PReLU().to(self.device) for _ in range(self.agents)])
+        self.fc2 = nn.ModuleList(
+            [nn.Linear(in_features=256, out_features=128).to(
+                self.device) for _ in range(self.agents)])
+        self.prelu5 = nn.ModuleList(
+            [nn.PReLU().to(self.device) for _ in range(self.agents)])
+        self.fc3 = nn.ModuleList(
+            [nn.Linear(in_features=128, out_features=number_actions).to(
+                self.device) for _ in range(self.agents)])
+
+        if xavier:
+            for module in self.modules():
+                if type(module) in [nn.Conv3d, nn.Linear]:
+                    torch.nn.init.xavier_uniform(module.weight)
+
+    def forward(self, input):
+        """
+        Input is a tensor of size
+        (batch_size, agents, frame_history, *image_size)
+        Output is a tensor of size
+        (batch_size, agents, number_actions)
+        """
+        input = input.to(self.device) / 255.0
+        output = []
+        for i in range(self.agents):
+            # Shared layers
+            x = input[:, i]
+            x = self.conv0(x)
+            x = self.prelu0(x)
+            x = self.maxpool0(x)
+            x = self.conv1(x)
+            x = self.prelu1(x)
+            x = self.maxpool1(x)
+            x = self.conv2(x)
+            x = self.prelu2(x)
+            x = self.maxpool2(x)
+            x = self.conv3(x)
+            x = self.prelu3(x)
+            x = x.view(-1, 512)
+            # Individual layers
+            x = self.fc1[i](x)
+            x = self.prelu4[i](x)
+            x = self.fc2[i](x)
+            x = self.prelu5[i](x)
+            x = self.fc3[i](x)
+            output.append(x)
+        output = torch.stack(output, dim=1)
+        return output.cpu()
+
+
+class CommNet(nn.Module):
+
+    def __init__(self, agents, frame_history, number_actions, xavier=True, attention=False):
+        super(CommNet, self).__init__()
+
+        self.agents = agents
+        self.frame_history = frame_history
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
+
+        self.conv0 = nn.Conv3d(
+            in_channels=frame_history,
+            out_channels=32,
+            kernel_size=(5, 5, 5),
+            padding=1).to(
+            self.device)
+        self.maxpool0 = nn.MaxPool3d(kernel_size=(2, 2, 2)).to(self.device)
+        self.prelu0 = nn.PReLU().to(self.device)
+        self.conv1 = nn.Conv3d(
+            in_channels=32,
+            out_channels=32,
+            kernel_size=(5, 5, 5),
+            padding=1).to(
+            self.device)
+        self.maxpool1 = nn.MaxPool3d(kernel_size=(2, 2, 2)).to(self.device)
+        self.prelu1 = nn.PReLU().to(self.device)
+        self.conv2 = nn.Conv3d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=(4, 4, 4),
+            padding=1).to(
+            self.device)
+        self.maxpool2 = nn.MaxPool3d(kernel_size=(2, 2, 2)).to(self.device)
+        self.prelu2 = nn.PReLU().to(self.device)
+        self.conv3 = nn.Conv3d(
+            in_channels=64,
+            out_channels=64,
+            kernel_size=(3, 3, 3),
+            padding=0).to(
+            self.device)
+        self.prelu3 = nn.PReLU().to(self.device)
+
+        self.fc1 = nn.ModuleList(
+            [nn.Linear(
+                in_features=512 * 2,
+                out_features=256).to(
+                self.device) for _ in range(
+                self.agents)])
+        self.prelu4 = nn.ModuleList(
+            [nn.PReLU().to(self.device) for _ in range(self.agents)])
+        self.fc2 = nn.ModuleList(
+            [nn.Linear(
+                in_features=256 * 2,
+                out_features=128).to(
+                self.device) for _ in range(
+                self.agents)])
+        self.prelu5 = nn.ModuleList(
+            [nn.PReLU().to(self.device) for _ in range(self.agents)])
+        self.fc3 = nn.ModuleList(
+            [nn.Linear(
+                in_features=128 * 2,
+                out_features=number_actions).to(
+                self.device) for _ in range(
+                self.agents)])
+
+        self.attention = attention
+        if self.attention:
+                self.comm_att1 = nn.ParameterList([nn.Parameter(torch.randn(agents)) for _ in range(agents)])
+                self.comm_att2 = nn.ParameterList([nn.Parameter(torch.randn(agents)) for _ in range(agents)])
+                self.comm_att3 = nn.ParameterList([nn.Parameter(torch.randn(agents)) for _ in range(agents)])
+
+        if xavier:
+            for module in self.modules():
+                if type(module) in [nn.Conv3d, nn.Linear]:
+                    torch.nn.init.xavier_uniform(module.weight)
+
+    def forward(self, input):
+        """
+        # Input is a tensor of size
+        (batch_size, agents, frame_history, *image_size)
+        # Output is a tensor of size
+        (batch_size, agents, number_actions)
+        """
+        input1 = input.to(self.device) / 255.0
+
+        # Shared layers
+        input2 = []
+        for i in range(self.agents):
+            x = input1[:, i]
+            x = self.conv0(x)
+            x = self.prelu0(x)
+            x = self.maxpool0(x)
+            x = self.conv1(x)
+            x = self.prelu1(x)
+            x = self.maxpool1(x)
+            x = self.conv2(x)
+            x = self.prelu2(x)
+            x = self.maxpool2(x)
+            x = self.conv3(x)
+            x = self.prelu3(x)
+            x = x.view(-1, 512)
+            input2.append(x)
+        input2 = torch.stack(input2, dim=1)
+         
+        # Communication layers
+        if self.attention:
+            comm = torch.cat([torch.sum((input2.transpose(1, 2) * nn.Softmax(dim=0)(self.comm_att1[i])), axis=2).unsqueeze(0)
+                              for i in range(self.agents)])
+            
+        else:
+            comm = torch.mean(input2, axis=1)
+            comm = comm.unsqueeze(0).repeat(self.agents, *[1]*len(comm.shape))
+        input3 = []
+        for i in range(self.agents):
+            x = input2[:, i]
+            x = self.fc1[i](torch.cat((x, comm[i]), axis=-1))
+            input3.append(self.prelu4[i](x))
+        input3 = torch.stack(input3, dim=1)
+
+        if self.attention:
+            comm = torch.cat([torch.sum((input3.transpose(1, 2) * nn.Softmax(dim=0)(self.comm_att2[i])), axis=2).unsqueeze(0)
+                              for i in range(self.agents)])
+        else:
+            comm = torch.mean(input3, axis=1)
+            comm = comm.unsqueeze(0).repeat(self.agents, *[1]*len(comm.shape))
+        input4 = []
+        for i in range(self.agents):
+            x = input3[:, i]
+            x = self.fc2[i](torch.cat((x, comm[i]), axis=-1))
+            input4.append(self.prelu5[i](x))
+        input4 = torch.stack(input4, dim=1)
+
+        if self.attention:
+            comm = torch.cat([torch.sum((input4.transpose(1, 2) * nn.Softmax(dim=0)(self.comm_att3[i])), axis=2).unsqueeze(0)
+                              for i in range(self.agents)])
+        else:
+            comm = torch.mean(input4, axis=1)
+            comm = comm.unsqueeze(0).repeat(self.agents, *[1]*len(comm.shape))
+        output = []
+        for i in range(self.agents):
+            x = input4[:, i]
+            x = self.fc3[i](torch.cat((x, comm[i]), axis=-1))
+            output.append(x)
+        output = torch.stack(output, dim=1)
+
+        return output.cpu()
+
+
+class DQN:
+    # The class initialisation function.
+    def __init__(
+            self,
+            agents,
+            frame_history,
+            logger,
+            number_actions=6,
+            type="Network3d",
+            collective_rewards=False,
+            attention=False,
+            lr=1e-3,
+            scheduler_gamma=0.9,
+            scheduler_step_size=100):
+        self.agents = agents
+        self.number_actions = number_actions
+        self.frame_history = frame_history
+        self.logger = logger
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
+        self.logger.log(f"Using {self.device}")
+        # Create a Q-network, which predicts the q-value for a particular state
+        if type == "Network3d":
+            self.q_network = Network3D(
+                agents,
+                frame_history,
+                number_actions).to(
+                self.device)
+            self.target_network = Network3D(
+                agents, frame_history, number_actions).to(
+                self.device)
+        elif type == "CommNet":
+            self.q_network = CommNet(
+                agents,
+                frame_history,
+                number_actions,
+                attention=attention).to(
+                self.device)
+            self.target_network = CommNet(
+                agents,
+                frame_history,
+                number_actions,
+                attention=attention).to(
+                self.device)
+        if collective_rewards == "attention":
+            self.q_network.rew_att = nn.Parameter(torch.randn(agents, agents))
+            self.target_network.rew_att = nn.Parameter(torch.randn(agents, agents))
+        self.copy_to_target_network()
+        # Freezes target network
+        self.target_network.train(False)
+        for p in self.target_network.parameters():
+            p.requires_grad = False
+        # Define the optimiser which is used when updating the Q-network. The
+        # learning rate determines how big each gradient step is during
+        # backpropagation.
+        self.optimiser = torch.optim.Adam(self.q_network.parameters(), lr=lr)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(
+            self.optimiser, step_size=scheduler_step_size, gamma=scheduler_gamma)
+        self.collective_rewards = collective_rewards
+
+    def copy_to_target_network(self):
+        self.target_network.load_state_dict(self.q_network.state_dict())
+
+    def save_model(self, name="dqn.pt", forced=False):
+        self.logger.save_model(self.q_network.state_dict(), name, forced)
+
+    # Function that is called whenever we want to train the Q-network. Each
+    # call to this function takes in a transition tuple containing the data we
+    # use to update the Q-network.
+    def train_q_network(self, transitions, discount_factor):
+        # Set all the gradients stored in the optimiser to zero.
+        self.optimiser.zero_grad()
+        # Calculate the loss for this transition.
+        loss = self._calculate_loss(transitions, discount_factor)
+        # Compute the gradients based on this loss, i.e. the gradients of the
+        # loss with respect to the Q-network parameters.
+        loss.backward()
+        # Take one gradient step to update the Q-network.
+        self.optimiser.step()
+        return loss.item()
+
+    # Function to calculate the loss for a particular transition.
+    def _calculate_loss(self, transitions, discount_factor):
+        '''
+        Transitions are tuple of shape
+        (states, actions, rewards, next_states, dones)
+        '''
+        curr_state = torch.tensor(transitions[0])
+        next_state = torch.tensor(transitions[3])
+        terminal = torch.tensor(transitions[4]).type(torch.int)
+
+        rewards = torch.clamp(
+            torch.tensor(
+                transitions[2], dtype=torch.float32), -1, 1)
+        # Collective rewards here refers to adding the (potentially weighted) average reward of all agents
+        if self.collective_rewards == "mean":
+            rewards += torch.mean(rewards, axis=1).unsqueeze(1).repeat(1, rewards.shape[1])
+        elif self.collective_rewards == "attention":
+            rewards = rewards + torch.matmul(rewards, nn.Softmax(dim=0)(self.q_network.rew_att))
+
+        y = self.target_network.forward(next_state)
+        # dim (batch_size, agents, number_actions)
+        y = y.view(-1, self.agents, self.number_actions)
+        # Get the maximum prediction for the next state from the target network
+        max_target_net = y.max(-1)[0]
+
+        # dim (batch_size, agents, number_actions)
+        network_prediction = self.q_network.forward(curr_state).view(
+            -1, self.agents, self.number_actions)
+        isNotOver = (torch.ones(*terminal.shape) - terminal)
+        # Bellman equation
+        batch_labels_tensor = rewards + isNotOver * \
+            (discount_factor * max_target_net.detach())
+
+        actions = torch.tensor(transitions[1], dtype=torch.long).unsqueeze(-1)
+        y_pred = torch.gather(network_prediction, -1, actions).squeeze()
+
+        return torch.nn.SmoothL1Loss()(batch_labels_tensor.flatten(), y_pred.flatten())
